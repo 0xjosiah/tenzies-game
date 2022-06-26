dc4aae18e1c11c88610ca555a750f789aaf69cdb
Josiah Webb <0xjosiah@MacBook-Pro.lan>
2022-06-26 17:16:55 -0600

    creates title and description, adds dice component, renders dice with random numbers, styles title and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './index.css';
+import Dice from './components/Dice'
 
 function App() {
+  const randomDieRoll = () => {
+    return Math.floor(Math.random() * 6) + 1
+  }
+
   return (
     <main className='main'>
+      <div className='game-title'>
+        <h1>Tenzies</h1>
+        <p>Roll until all dice are the same. Click each die 
+        to freeze it at its current value between rolls.</p>
+      </div>
+      <Dice value={randomDieRoll} />
     </main>
   );
 }
